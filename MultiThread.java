ccf71223dd3dd5150495806f6598a7c97e17234b
Anisha <anishamohanty23@gmail.com>
2018-08-07 00:53:32 +0530

    Creating thread by extending Thread Class

---
+++ MultiThread.java
@@ -0,0 +1,10 @@
+
+public class MultiThread extends Thread {
+	public void run(){  
+		System.out.println("Thread is running...");  
+		}  
+		public static void main(String args[]){  
+		MultiThread t1=new MultiThread();  
+		t1.start();  
+		 }  
+}
